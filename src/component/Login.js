b37199a7b5be01fa34029eed9450853accc103f5
Adish Dahal <me.adishdahal@gmail.com>
2022-11-12 15:22:46 +0545

    fixed login page

--- src/component/Login.js
+++ src/component/Login.js
@@ -8,65 +8,177 @@ import Header from "./include/Header";
 export default function Login() {
   return (
     <>
-      <div className="grid sm:grid-cols-2 sm:bg-[#161C24]">
-
+      <div
+        className="
+        tdc:bg-[#161C24]
+        bg-transparent
+        grid  
+        tdc:grid-cols-2      
+        space-y-2  fdc:space-y-5 tdc:space-y-2
+      "
+      >
         <Header />
 
-        <div className="p-5 lmd:px-20 lmd:py-16 lg:px-32 lg:py-28 md:p-5 md:px-10 sm:space-y-4 space-y-3">
-
-          <div className="grid space-y-2">
-            <div className="justify-self-center  font-bold sm:text-[#E4E6F2] text-4xl ">
+        <div
+          className="
+        p-2
+        space-y-2
+        fdc:place-self-center
+        fdc:min-w-[320px]
+        tdc:min-w-[300px]
+        ftdc:min-w-[330px]
+        fvdc:min-w-[420px]
+        "
+        >
+          <div
+            className="
+          grid
+          space-y-1  ftdc:space-y-3
+          "
+          >
+            <div
+              className="
+            justify-self-center
+            font-bold tdc:text-[#E4E6F2]
+            text-xl fdc:text-3xl tdc:text-2xl ftdc:text-4xl
+            "
+            >
               Login
             </div>
-            <div className="justify-self-center  sm:text-[#A6A7B0] ">
+
+            <div
+              className="
+            justify-self-center  tdc:text-[#E4E6F2]
+            text-xs fdc:text-sm tdc:text-xs
+            "
+            >
               <span>Needs an account?</span>
-              <span className="text-[#F2994A] font-bold">
+              <span
+                className="
+              text-[#F2994A] 
+              font-bold
+              "
+              >
                 <a href="#"> Sign Up</a>
               </span>
             </div>
           </div>
 
-          <form className="sm:space-y-3 space-y-2">
+          <form
+            className="
+          space-y-2
+  
+          "
+          >
             <div className="">
-              <label className="sm:text-[#DEDEDF]">
+              <label
+                className="
+              text-sm fdc:text-md tdc:text-sm tdc:text-[#E4E6F2]
+              "
+              >
                 Email <span className="text-[#D44453]">*</span>
               </label>
               <br />
               <input
                 type="text"
-                className="outline-2 p-2   outline-[#F2994A] bg-[#384063] text-white w-full   rounded-md"
+                className="
+                  outline-2
+                  outline-[#F2994A]
+                  bg-[#384063] t
+                  text-white 
+                  w-full
+                  py-1 fdc:p-2 tdc:py-1  fvdc:p-2 
+                  rounded-md
+                "
               />
             </div>
             <div className="">
-              <label className="sm:text-[#DEDEDF]">
+              <label
+                className="
+              text-sm fdc:text-md tdc:text-sm tdc:text-[#E4E6F2]
+              "
+              >
                 Password <span className="text-[#D44453]">*</span>
               </label>
               <br />
               <div className="relative">
-                <input type='text' className="text-md block outline-2 p-2   outline-[#F2994A] bg-[#384063] text-white w-full   rounded-md"  ></input>
-                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-                  <img src={Show} className="h-6 text-gray-700 cursor-pointer"></img>
+                <input
+                  type="text"
+                  className="
+                outline-2
+                outline-[#F2994A]
+                bg-[#384063] t
+                text-white 
+                w-full
+                py-1 fdc:p-2  tdc:py-1 fvdc:p-2
+                rounded-md
+                block"
+                ></input>
+
+                <div className="
+                absolute 
+                inset-y-0 
+                right-0 
+                pr-3 
+                flex 
+                items-center 
+                text-sm 
+                leading-5
+                ">
+                  <img
+                    src={Show}
+                    className="
+                    h-4
+                   text-gray-700 
+                   cursor-pointer"
+                  ></img>
                 </div>
               </div>
             </div>
 
-            <div className="grid justify-items-end text-[#F2994A] font-bold">
+            <div className="
+            grid 
+            justify-items-end 
+            text-[#F2994A] 
+            font-semibold
+            text-sm
+            ">
               <a href=""> Forgot your Password? </a>
             </div>
-          </form >
+          </form>
 
-
-
-          <div className="space-y-4">
-            <button className="w-full p-2 bg-[#F2994A] text-white rounded-md text-center  ">
+          <div className="
+          space-y-2
+          ">
+            <button className="
+            w-full 
+            p-1  fdc:p-2 tdc:p-1 fvdc:p-2
+            bg-[#F2994A] 
+            text-white 
+            rounded-md 
+            text-center
+            ">
               Login
             </button>
-            <button className="w-full p-2 flex space-x-2  bg-white text-[#F2994A] border border-[#F2994A] sm:border-none rounded-md justify-center">
+            <button className="
+            w-full  
+            p-1 fdc:p-2 tdc:p-1 fvdc:p-2
+            flex 
+            space-x-2  
+            bg-white 
+            text-[#F2994A] 
+            border 
+            border-[#F2994A] 
+            sm:border-none 
+            rounded-md 
+            justify-center
+            ">
               <img src={Google}></img> <span>Sign-in with Google</span>
             </button>
           </div>
-        </div >
-      </div >
+        </div>
+      </div>
+   
     </>
   );
 }
