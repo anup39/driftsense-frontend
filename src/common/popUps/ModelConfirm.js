f8fe18bd65042a6f8cdacfe9f8817fef24677cf3
Adish Dahal <adish@adish.adishdahal.com>
2022-11-24 09:01:32 -0500

    Height Fixed

--- src/common/popUps/ModelConfirm.js
+++ src/common/popUps/ModelConfirm.js
@@ -6,7 +6,7 @@ export default function ModelConfirm() {
   return (
     <>
       <div className="w-full absolute   h-screen grid place-content-center ">
-        <div className="bg-slate-800 p-4 max-w-[18.69rem] max-h-[144px] rounded-lg space-y-5  mx-auto ">
+        <div className="bg-slate-800 p-4 max-w-[18.69rem] min-h-[144px] rounded-lg space-y-5  mx-auto ">
           <ConfirmModelHeader />
           <div className="flex flex-row gap-4 justify-center items-center">
             <PopUpCancel />
